d907e4f7bb4caa02d7352668e65653940b428f33
vinyasbj <vinyas555@gmail.com>
2019-12-11 00:07:54 +0530

    new changes

--- app/models/membership_form.rb
+++ app/models/membership_form.rb
@@ -2,6 +2,16 @@ class MembershipForm < ApplicationRecord
     belongs_to :user,optional: true
     belongs_to :admin,optional: true, :class_name => "User", :foreign_key => 'admin_id'
     mount_uploader :image, ImageUploader
-    validates_uniqueness_of :user_id
+    # validates_uniqueness_of :user_id
     scope :admin_forms ,-> {where.not(admin_id: nil)}
+
+    before_update :change_status
+
+    def change_status
+        if self.status_changed?
+            binding.pry
+            StatusMailer.status_change(self).deliver_now
+        end
+        binding.pry
+    end
 end
